f59e9bc1d07a293fabc4ecdb4259ccac0312400e
gusang <52649646+rookiiiie@users.noreply.github.com>
2024-08-08 00:33:40 +0800

    Update RpcMessageEncoder.java

--- rpc-framework-simple/src/main/java/github/javaguide/remoting/transport/netty/codec/RpcMessageEncoder.java
+++ rpc-framework-simple/src/main/java/github/javaguide/remoting/transport/netty/codec/RpcMessageEncoder.java
@@ -14,6 +14,7 @@ import io.netty.handler.codec.MessageToByteEncoder;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 
 /**
@@ -21,17 +22,17 @@ import java.util.concurrent.atomic.AtomicInteger;
  * custom protocol decoder
  * <p>
  * <pre>
- *   0     1     2     3     4        5     6     7     8         9          10      11     12  13  14   15 16
- *   +-----+-----+-----+-----+--------+----+----+----+------+-----------+-------+----- --+-----+-----+-------+
- *   |   magic   code        |version | full length         | messageType| codec|compress|    RequestId       |
- *   +-----------------------+--------+---------------------+-----------+-----------+-----------+------------+
- *   |                                                                                                       |
- *   |                                         body                                                          |
- *   |                                                                                                       |
- *   |                                        ... ...                                                        |
- *   +-------------------------------------------------------------------------------------------------------+
- * 4B  magic code（魔法数）   1B version（版本）   4B full length（整个消息长度,body最大4048-16byte）    1B messageType（消息类型）
- * 1B compress（压缩类型） 1B codec（序列化类型）    4B  requestId（请求的Id）
+ *   0     1     2     3     4        5     6     7     8         9          10      11     12  13  14                20
+ *   +-----+-----+-----+-----+--------+----+----+----+------+-----------+-------+----- --+-----+-----+-----------------+
+ *   |   magic   code        |version | full length         | messageType| codec|compress|           RequestId         |
+ *   +-----------------------+--------+---------------------+-----------+-----------+-----------+----------------------+
+ *   |                                                                                                                 |
+ *   |                                         body                                                                    |
+ *   |                                                                                                                 |
+ *   |                                        ... ...                                                                  |
+ *   +-----------------------------------------------------------------------------------------------------------------+
+ * 4B  magic code（魔法数）   1B version（版本）   4B full length（消息长度）    1B messageType（消息类型）
+ * 1B compress（压缩类型） 1B codec（序列化类型）    8B  requestId（请求的Id）
  * body（object类型数据）
  * </pre>
  *
@@ -42,7 +43,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 public class RpcMessageEncoder extends MessageToByteEncoder<RpcMessage> {
-    private static final AtomicInteger ATOMIC_INTEGER = new AtomicInteger(0);
+    private static final AtomicLong ATOMIC_Long = new AtomicLong(0);
 
     @Override
     protected void encode(ChannelHandlerContext ctx, RpcMessage rpcMessage, ByteBuf out) {
@@ -55,7 +56,7 @@ public class RpcMessageEncoder extends MessageToByteEncoder<RpcMessage> {
             out.writeByte(messageType);
             out.writeByte(rpcMessage.getCodec()); //SerializationTypeEnum.HESSIAN.getCode()
             out.writeByte(CompressTypeEnum.GZIP.getCode());
-            out.writeInt(ATOMIC_INTEGER.getAndIncrement());
+            out.writeLong(ATOMIC_Long.getAndIncrement()); //这里为了测试，直接简单搞了个请求id
             // build full length
             byte[] bodyBytes = null;
             int fullLength = RpcConstants.HEAD_LENGTH;
