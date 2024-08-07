b71c7f9a74008840aafdcdb852b9d00c602caad1
gusang <52649646+rookiiiie@users.noreply.github.com>
2024-08-08 00:33:03 +0800

    Update RpcMessageDecoder.java

--- rpc-framework-simple/src/main/java/github/javaguide/remoting/transport/netty/codec/RpcMessageDecoder.java
+++ rpc-framework-simple/src/main/java/github/javaguide/remoting/transport/netty/codec/RpcMessageDecoder.java
@@ -19,17 +19,17 @@ import java.util.Arrays;
 /**
  * custom protocol decoder
  * <pre>
- *   0     1     2     3     4        5     6     7     8         9          10      11     12  13  14   15 16
- *   +-----+-----+-----+-----+--------+----+----+----+------+-----------+-------+----- --+-----+-----+-------+
- *   |   magic   code        |version | full length         | messageType| codec|compress|    RequestId       |
- *   +-----------------------+--------+---------------------+-----------+-----------+-----------+------------+
- *   |                                                                                                       |
- *   |                                         body                                                          |
- *   |                                                                                                       |
- *   |                                        ... ...                                                        |
- *   +-------------------------------------------------------------------------------------------------------+
+ *   0     1     2     3     4        5     6     7     8         9          10      11     12  13  14                20
+ *   +-----+-----+-----+-----+--------+----+----+----+------+-----------+-------+----- --+-----+-----+-----------------+
+ *   |   magic   code        |version | full length         | messageType| codec|compress|           RequestId         |
+ *   +-----------------------+--------+---------------------+-----------+-----------+-----------+----------------------+
+ *   |                                                                                                                 |
+ *   |                                         body                                                                    |
+ *   |                                                                                                                 |
+ *   |                                        ... ...                                                                  |
+ *   +-----------------------------------------------------------------------------------------------------------------+
  * 4B  magic code（魔法数）   1B version（版本）   4B full length（消息长度）    1B messageType（消息类型）
- * 1B compress（压缩类型） 1B codec（序列化类型）    4B  requestId（请求的Id）
+ * 1B compress（压缩类型） 1B codec（序列化类型）    8B  requestId（请求的Id）
  * body（object类型数据）
  * </pre>
  * <p>
@@ -96,7 +96,7 @@ public class RpcMessageDecoder extends LengthFieldBasedFrameDecoder {
         byte messageType = in.readByte();
         byte codecType = in.readByte();
         byte compressType = in.readByte();
-        int requestId = in.readInt();
+        long requestId = in.readLong();
         RpcMessage rpcMessage = RpcMessage.builder()
                 .codec(codecType)
                 .requestId(requestId)
